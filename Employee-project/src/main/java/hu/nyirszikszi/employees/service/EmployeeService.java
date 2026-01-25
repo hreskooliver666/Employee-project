515768762cc78d6a707bcc91b95a98efd8b45c8f
hreskooliver666 <21c_hreskoo@nyirszikszi.hu>
2026-01-25 17:23:29 +0100

    Add create method with duplicate email check to EmployeeService

--- Employee-project/src/main/java/hu/nyirszikszi/employees/service/EmployeeService.java
+++ Employee-project/src/main/java/hu/nyirszikszi/employees/service/EmployeeService.java
@@ -1,7 +1,10 @@
 package hu.nyirszikszi.employees.service;
 
+import hu.nyirszikszi.employees.domain.Department;
 import hu.nyirszikszi.employees.domain.Employee;
+import hu.nyirszikszi.employees.dto.CreateEmployeeCommand;
 import hu.nyirszikszi.employees.dto.EmployeeDto;
+import hu.nyirszikszi.employees.exception.DuplicateEmailException;
 import hu.nyirszikszi.employees.repository.InMemoryEmployeeRepository;
 
 
@@ -13,7 +16,22 @@ public class EmployeeService {
         this.repo = repo;
     }
 
-    
+    public EmployeeDto create(CreateEmployeeCommand cmd) {
+        repo.findByEmailIgnoreCase(cmd.getEmail()).ifPresent( e -> {
+            throw new DuplicateEmailException(cmd.getEmail());
+        });
+
+        Employee saved = repo.save(new Employee(
+                0,
+                cmd.getName().trim(),
+                cmd.getEmail().trim(),
+                cmd.getSalary(),
+                cmd.getBirthDate(),
+                Department.valueOf(cmd.getDepartment().trim().toUpperCase())
+        ));
+        return toDto(saved);
+    }
+
     private EmployeeDto toDto(Employee e) {
         return new EmployeeDto(
                 e.getId(),
