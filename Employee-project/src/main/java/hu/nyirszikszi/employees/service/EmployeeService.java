3c36955ce33c97983d5a38bd8c929e6ef31be397
hreskooliver666 <21c_hreskoo@nyirszikszi.hu>
2026-01-25 17:22:32 +0100

    Add toDto mapper method to EmployeeService

--- Employee-project/src/main/java/hu/nyirszikszi/employees/service/EmployeeService.java
+++ Employee-project/src/main/java/hu/nyirszikszi/employees/service/EmployeeService.java
@@ -1,5 +1,7 @@
 package hu.nyirszikszi.employees.service;
 
+import hu.nyirszikszi.employees.domain.Employee;
+import hu.nyirszikszi.employees.dto.EmployeeDto;
 import hu.nyirszikszi.employees.repository.InMemoryEmployeeRepository;
 
 
@@ -10,6 +12,17 @@ public class EmployeeService {
     public EmployeeService(InMemoryEmployeeRepository repo) {
         this.repo = repo;
     }
+
     
+    private EmployeeDto toDto(Employee e) {
+        return new EmployeeDto(
+                e.getId(),
+                e.getName(),
+                e.getEmail(),
+                e.getSalary(),
+                e.getBirthDate(),
+                e.getDepartment()
+        );
+    }
 
 }
