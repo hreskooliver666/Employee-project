d9ee5556d415580327c107ff0e070e202058c546
hreskooliver666 <21c_hreskoo@nyirszikszi.hu>
2026-01-25 16:39:41 +0100

    Define fields in UpdateEmployeeCommand

--- Employee-project/src/main/java/hu/nyirszikszi/employees/dto/UpdateEmployeeCommand.java
+++ Employee-project/src/main/java/hu/nyirszikszi/employees/dto/UpdateEmployeeCommand.java
@@ -1,5 +1,14 @@
 package hu.nyirszikszi.employees.dto;
 
+import java.time.LocalDate;
+
 public class UpdateEmployeeCommand {
+
+    private String name;
+    private String email;
+    private Integer salary;
+    private LocalDate birthDate;
+    private String department;
     
+
 }
