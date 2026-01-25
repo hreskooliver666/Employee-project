3479e805bdb55af9b65450d2ff25b54755ed23e1
hreskooliver666 <21c_hreskoo@nyirszikszi.hu>
2026-01-25 16:22:14 +0100

    Define fields in CreateEmployeeCommand

--- Employee-project/src/main/java/hu/nyirszikszi/employees/dto/CreateEmployeeCommand.java
+++ Employee-project/src/main/java/hu/nyirszikszi/employees/dto/CreateEmployeeCommand.java
@@ -1,5 +1,13 @@
 package hu.nyirszikszi.employees.dto;
 
+import java.time.LocalDate;
+
 public class CreateEmployeeCommand {
     
+    private String name;
+    private String email;
+    private Integer salary;
+    private LocalDate birthDate;
+    private String department;
+
 }
