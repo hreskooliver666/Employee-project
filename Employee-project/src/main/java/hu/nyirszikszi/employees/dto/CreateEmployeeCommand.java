16a9b63e6989171486943d3097f7de0e9da5dc7c
hreskooliver666 <21c_hreskoo@nyirszikszi.hu>
2026-01-25 16:26:43 +0100

    Add basic validation annotations to CreateEmployeeCommand

--- Employee-project/src/main/java/hu/nyirszikszi/employees/dto/CreateEmployeeCommand.java
+++ Employee-project/src/main/java/hu/nyirszikszi/employees/dto/CreateEmployeeCommand.java
@@ -1,13 +1,28 @@
 package hu.nyirszikszi.employees.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+
 import java.time.LocalDate;
 
 public class CreateEmployeeCommand {
 
+
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email format is invalid")
     private String email;
+
+    @Min(value = 1, message = "Salary must be at least 1")
     private Integer salary;
+
+    @Past(message = "Birth date must be in the past")
     private LocalDate birthDate;
+
     private String department;
 
 
