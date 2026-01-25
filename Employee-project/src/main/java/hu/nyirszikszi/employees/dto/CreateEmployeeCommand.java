93cde76378fbebcd5389cbfd079c0c5fe8f5a049
hreskooliver666 <21c_hreskoo@nyirszikszi.hu>
2026-01-25 16:24:17 +0100

    Add getters to CreateEmployeeCommand

--- Employee-project/src/main/java/hu/nyirszikszi/employees/dto/CreateEmployeeCommand.java
+++ Employee-project/src/main/java/hu/nyirszikszi/employees/dto/CreateEmployeeCommand.java
@@ -3,11 +3,34 @@ package hu.nyirszikszi.employees.dto;
 import java.time.LocalDate;
 
 public class CreateEmployeeCommand {
-    
+
     private String name;
     private String email;
     private Integer salary;
     private LocalDate birthDate;
     private String department;
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
 
 }
