29205f7d67a01ba4006270b5073b12a7a7b5fed0
hreskooliver666 <21c_hreskoo@nyirszikszi.hu>
2026-01-25 17:21:25 +0100

    Inject InMemoryEmployeeRepository into EmployeeService

--- Employee-project/src/main/java/hu/nyirszikszi/employees/service/EmployeeService.java
+++ Employee-project/src/main/java/hu/nyirszikszi/employees/service/EmployeeService.java
@@ -1,5 +1,15 @@
 package hu.nyirszikszi.employees.service;
 
+import hu.nyirszikszi.employees.repository.InMemoryEmployeeRepository;
+
+
 public class EmployeeService {
+
+    private final InMemoryEmployeeRepository repo;
+
+    public EmployeeService(InMemoryEmployeeRepository repo) {
+        this.repo = repo;
+    }
     
+
 }
