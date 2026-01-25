f41539c9c6f407522de3d4ca86a98212a5a94ac2
hreskooliver666 <21c_hreskoo@nyirszikszi.hu>
2026-01-25 16:51:43 +0100

    Add in-memory store and id sequence to InMemoryEmployeeRepository

--- Employee-project/src/main/java/hu/nyirszikszi/employees/repository/InMemoryEmployeeRepository.java
+++ Employee-project/src/main/java/hu/nyirszikszi/employees/repository/InMemoryEmployeeRepository.java
@@ -1,5 +1,15 @@
 package hu.nyirszikszi.employees.repository;
 
+import hu.nyirszikszi.employees.domain.Employee;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
 public class InMemoryEmployeeRepository {
-    
+
+    private final Map<Long, Employee> store = new ConcurrentHashMap<>();
+    private final AtomicLong seq = new AtomicLong(0);
+
+
 }
