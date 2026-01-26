edbe4e560a946848e1fa1f1513816ab1bf494042
Proof <21c_hreskoo@nyirszikszi.hu>
2026-01-26 09:12:52 +0100

    Add method create in EmployeeControlller.java

--- Employee-project/src/main/java/hu/nyirszikszi/employees/controller/EmployeeController.java
+++ Employee-project/src/main/java/hu/nyirszikszi/employees/controller/EmployeeController.java
@@ -1,11 +1,28 @@
 package hu.nyirszikszi.employees.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import hu.nyirszikszi.employees.dto.CreateEmployeeCommand;
+import hu.nyirszikszi.employees.dto.EmployeeDto;
+import hu.nyirszikszi.employees.service.EmployeeService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/employees")
 public class EmployeeController {
+    private final EmployeeService service;
+
+
+    public EmployeeController(EmployeeService service) {
+        this.service = service;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public EmployeeDto create (@Valid @RequestBody CreateEmployeeCommand command){
+        return service.create(command);
+    }
+    
 
 
 }
