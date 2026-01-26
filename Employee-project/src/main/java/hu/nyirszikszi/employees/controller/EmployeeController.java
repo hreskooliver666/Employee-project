b3fac193772e04b865c626593d966c67b17c2e48
Proof <21c_hreskoo@nyirszikszi.hu>
2026-01-26 09:03:44 +0100

    Add default annotations to EmployeeController.java

--- Employee-project/src/main/java/hu/nyirszikszi/employees/controller/EmployeeController.java
+++ Employee-project/src/main/java/hu/nyirszikszi/employees/controller/EmployeeController.java
@@ -1,4 +1,11 @@
 package hu.nyirszikszi.employees.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/employees")
 public class EmployeeController {
+
+
 }
