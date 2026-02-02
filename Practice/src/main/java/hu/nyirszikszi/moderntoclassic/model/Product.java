40f23945ed6fb61f824565921e863e6571023422
Proof <21c_hreskoo@nyirszikszi.hu>
2026-02-02 09:55:00 +0100

    Add constructor and getters and toString to Product class

--- Practice/src/main/java/hu/nyirszikszi/moderntoclassic/model/Product.java
+++ Practice/src/main/java/hu/nyirszikszi/moderntoclassic/model/Product.java
@@ -1,4 +1,43 @@
 package hu.nyirszikszi.moderntoclassic.model;
 
 public class Product {
+
+    private final String sku;
+    private final String name;
+    private Category category;
+    private final int price;
+
+    public Product(String sku, String name, Category category, int price) {
+        this.sku = sku;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "sku='" + sku + '\'' +
+                ", name='" + name + '\'' +
+                ", category=" + category +
+                ", price=" + price +
+                '}';
+    }
 }
