38bd261e0fdf58d052db3606c90c5a8325a6a8dc
Proof <21c_hreskoo@nyirszikszi.hu>
2026-02-02 10:01:04 +0100

    Add findBySkuIgnoreCase method in CartPracticeModern

--- Practice/src/main/java/hu/nyirszikszi/moderntoclassic/CartPracticeModern.java
+++ Practice/src/main/java/hu/nyirszikszi/moderntoclassic/CartPracticeModern.java
@@ -1,9 +1,29 @@
 package hu.nyirszikszi.moderntoclassic;
 
+import hu.nyirszikszi.moderntoclassic.model.Product;
+
 import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
 
 public class CartPracticeModern {
 
     private final List<Product> catalog;
+
+    public CartPracticeModern(List<Product> catalog) {
+        this.catalog = catalog;
+    }
+
+    Optional<Product> findBySkuIgnoreCase(String sku){
+        if(sku == null) return Optional.empty();
+        String needle = sku.toLowerCase(Locale.ROOT);
+
+        return catalog.stream()
+                .filter(p -> p.getSku() != null)
+                .filter(p -> p.getSku() .trim().toLowerCase(Locale.ROOT).equals(needle))
+                .findFirst();
+
+    }
+
 
 }
