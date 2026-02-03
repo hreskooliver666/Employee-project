ce0785022a5b736f098cbfa8f8f41a1c27de94f1
Proof <21c_hreskoo@nyirszikszi.hu>
2026-02-03 12:37:04 +0100

    Add findCheapestInCategory method to CartPracticeModernSolution

--- Practice/src/main/java/hu/nyirszikszi/classictomodern/CartPracticeModernSolution.java
+++ Practice/src/main/java/hu/nyirszikszi/classictomodern/CartPracticeModernSolution.java
@@ -1,4 +1,23 @@
 package hu.nyirszikszi.classictomodern;
 
+import hu.nyirszikszi.model.Category;
+import hu.nyirszikszi.model.Product;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
 public class CartPracticeModernSolution {
+    private final List<Product> catalog;
+
+    public CartPracticeModernSolution(List<Product> catalog) {
+        this.catalog = catalog;
+    }
+
+    public Optional<Product> findCheapestInCategory(Category category){
+        return catalog.stream()
+                .filter(p -> p.getCategory() == category)
+                .min(Comparator.comparingInt(Product::getPrice));
+    }
+
 }
