bd8e8a31fdbb38d11fe0d1b406fb0dc81744102a
Proof <21c_hreskoo@nyirszikszi.hu>
2026-02-03 12:31:56 +0100

    Add findCheapestInCategoryOrNull method to CartPracticeClassic

--- Practice/src/main/java/hu/nyirszikszi/classictomodern/CartPracticeClassic.java
+++ Practice/src/main/java/hu/nyirszikszi/classictomodern/CartPracticeClassic.java
@@ -1,5 +1,29 @@
 package hu.nyirszikszi.classictomodern;
 
+import hu.nyirszikszi.model.Category;
+import hu.nyirszikszi.model.Product;
+
+import java.util.List;
+
 public class CartPracticeClassic {
-    
+
+    private final List<Product> catalog;
+
+
+    public CartPracticeClassic(List<Product> catalog) {
+        this.catalog = catalog;
+    }
+
+    public Product findCheapestInCategoryOrNull(Category category){
+        Product best = null;
+        for(Product p : catalog){
+            if(p.getCategory() == category){
+                if(best == null || p.getPrice() < best.getPrice()){
+                    best = p;
+                }
+            }
+        }
+        return best;
+    }
+
 }
